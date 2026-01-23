9dfa224f426d0a23f19d67906b29efa123591278
Sirawich Pipatprathanporn <spipatprathanporn@ucsd.edu>
2026-01-23 15:11:57 -0800

    List the names of CAATEX moorings

---
+++ listcaatexmoorings.m
@@ -0,0 +1,11 @@
+function names = listcaatexmoorings
+% names = LISTCAATEXMOORINGS
+%
+% List mooring names for CAATEX Experiment.
+%
+% OUTPUT:
+% names     cell array containing mooring names
+%
+% Last modified by spipatprathanporn@ucsd.edu, 01/23/2026
+
+names = {'NERSC1', 'NERSC2', 'NERSC3', 'SIO1', 'SIO2', 'SIO3'};
