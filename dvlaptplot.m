7155535709ff608a22a11a71f9e8ab373dbeb00a
Sirawich Pipatprathanporn <spipatprathanporn@ucsd.edu>
2026-02-09 18:20:40 -0800

    bug fixes

--- dvlaptplot.m
+++ dvlaptplot.m
@@ -13,19 +13,25 @@ function fig = dvlaptplot(z, t, data, c)
 % OUTPUT:
 % fig       figure handle
 %
-% Last modified by spipatprathanporn@ucsd.edu, 01/30/2026
+% Last modified by spipatprathanporn@ucsd.edu, 02/09/2026
 
-% slownesses
+% horizontal slownesses
 p = (-1:0.01:1)' * 1/c;
+% vertical slowness
+eta = sqrt(1/c^2 - p.^2);
+
+% moving window is 10 seconds long
+fs = 1 / (t(2) - t(1));
+N = round(10*fs);
 
 pt = zeros([length(p) length(t)]);
 
 for jj = 1:length(p)
     xs = data(1,:);
-    for ii = 2:length(list)
-        xs = xs + interp1(dt + seconds(sign(p(jj)) * ...
-            sqrt(1/c^2 - pp(jj)^2) * (z(ii) - z(1))), ...
-            data(ii,:), dt, 'linear', 0);
+    for ii = 2:length(z)
+        xs = xs + interp1(t + sign(p(jj)) * ...
+            eta(jj) * (z(ii) - z(1)), ...
+            data(ii,:), t, 'linear', 0);
     end
     xs_mvar = movvar(xs, N);
     pt(jj,:) = xs_mvar;
@@ -34,7 +40,7 @@ end
 fig = figure(2);
 clf
 set(gcf, 'Units', 'inches', 'Position', [0 1 8 6])
-imagesc([t(1) t(end)], [p(1) p(end)], pt)
+imagesc([t(1) t(end)], [p(1) p(end)], log10(pt))
 axis xy
 grid on
 set(gca, 'FontSize', 12, 'TickDir', 'out', 'Box', 'on')
@@ -42,7 +48,7 @@ xlabel('time (s)')
 ylabel('horizontal slowness (s/m)')
 
 cb = colorbar;
-set(cb.Label, 'String', '10-s moving variance')
+set(cb.Label, 'String', 'log_{10} 10-s moving variance')
 set(cb, 'TickDirection', 'both')
 set(gcf, 'Renderer', 'painters')
 end
