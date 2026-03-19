72009d4d7324edf38eca6d5607ca20b8294cf58e
Sirawich Pipatprathanporn <spipatprathanporn@ucsd.edu>
2026-03-19 14:46:41 -0700

    Also split row vectors + remove all 0-size sections

--- splitnan.m
+++ splitnan.m
@@ -1,7 +1,7 @@
 function C = splitnan(M)
 % C = SPLITNAN(M)
 %
-% Split the matrix M at all-NaN rows. See
+% Splits the matrix M at all-NaN rows unless M is a row vector. See
 % https://www.mathworks.com/matlabcentral/answers/
 % 335333-split-array-into-separate-arrays-at-row-with-nans
 % for the method.
@@ -17,15 +17,25 @@ function C = splitnan(M)
 % C = splitnan(M);
 %
 % % Expected result
-% % C = {[1 2 3; 4 5 6], [7 8 9; 10 NaN 12; 13 14 15})
+% % C = {[1 2 3; 4 5 6]; [7 8 9; 10 NaN 12; 13 14 15})
 %
 % SEE ALSO:
 % ACCUMARRAY, SPLIT
 %
 % Last modified by spipatprathanporn@ucsd.edu, 03/19/2026
 
-idx = all(isnan(M), 2);
-idy = 1 + cumsum(idx);
-idz = 1:size(M, 1);
-C = accumarray(idy(~idx), idz(~idx), [], @(r) {M(r,:)});
+if isrow(M)
+    idx = isnan(M);
+    idy = 1 + cumsum(idx);
+    idz = 1:length(M);
+    C = accumarray(idy(~idx)', idz(~idx), [], @(r) {M(r)});
+else
+    idx = all(isnan(M), 2);
+    idy = 1 + cumsum(idx);
+    idz = 1:size(M, 1);
+    C = accumarray(idy(~idx), idz(~idx), [], @(r) {M(r,:)});
+end
+
+% remove empty elements
+C = C(~cellfun(@isempty, C));
 end
