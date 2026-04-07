dcfff4adcf0ca0582ab8338b279308b5e1278c8e
Sirawich Pipatprathanporn <spipatprathanporn@ucsd.edu>
2026-04-06 19:39:51 -0700

    Aligned time samples of all seismograms + made plots more legible

--- dvlafkplot.m
+++ dvlafkplot.m
@@ -9,12 +9,13 @@ function dvlafkplot(station, datadir, metadatadir)
 % datadir       directory containing data from all stations
 % metadatadir   directory containing metadata for the station
 %
-% Last modified by spipatprathanporn@ucsd.edu, 12/19/2025
+% Last modified by spipatprathanporn@ucsd.edu, 04/06/2026
 
 % TODO: change to environment variables
 % TODO: decide where to host data+metadata
 defval('metadatadir', '/Volumes/aog/AOGquake/work/Ivy/CAATEX/metadata');
 defval('datadir', '/Volumes/aog/AOGquake/vault/CAATEX/')
+addpath(genpath(metadatadir))
 
 % determine which directories / metadata files to read
 if strcmpi(station, 'SIO1')
@@ -76,29 +77,22 @@ end
 % loop over all sections
 for ii = 1:fndex
     fname = removepath(allfiles{ii});
+    
+    % construct beginning and ending datetimes
+    jday = str2double(fname(5:7));
+    jhour = str2double(fname(8:9));
+    jminute = str2double(fname(10:11));
+    jsecond = str2double(fname(12:13));
+    dt_begin = datetime(2018,12,31,jhour,jminute,jsecond+1, ...
+        'Format', 'uuuu-MM-dd''T''HH:mm:ss.SSSSSS') + days(jday);
+    dt_end = dt_begin + seconds(2710-1);
 
-    % determine length of time series data, time and space sampling
-    [x, t, jdn] = HMrcvx(allfiles{ii});
-    N = length(t);
+    % request data from all hydrophones from the mooring
+    [t, xx, jdn, num, depth] = getdvlaseis(station, 'all', dt_begin, ...
+        dt_end, 100, datadir, metadatadir);
     dt = t(2) - t(1);
     dz = hmdep(2) - hmdep(1);
-    
-    % construct 2D array storing signals from the hydrophone array
-    xx = zeros([N length(snx)]);
-    xx(:,1) = x';
-
-    % loop over the hydrophones
-    for jj = 2:length(snx)
-        try
-            x = HMrcvx(sprintf('%s/%03d/%s', masterdir, snx(jj), fname));
-            xx(:,jj) = x';
-        catch ME
-            % if cannot read file, use data from the previous hydrophone
-            ME.getReport()
-            xx(:,jj) = xx(:,jj-1);
-            continue
-        end
-    end
+    xx = xx';
 
     % F-K transformation
     [XX, F, K] = fk(xx, dt, dz);
@@ -113,7 +107,7 @@ for ii = 1:fndex
     axis xy
     xlabel('wavenumber (m^{-1})')
     ylabel('frequency (Hz)')
-    title(sprintf('station: %s - %s', station, datestr(jdn)))
+    title(sprintf('station: %s - %s', station, dt_begin))
     cb = colorbar;
     cb.TickDirection = 'out';
     cb.Label.String = '10 log_{10}(spectral density) (dB)';
@@ -126,7 +120,7 @@ for ii = 1:fndex
     xlabel('wavenumber (m^{-1})')
     ylabel('frequency (Hz)')
     title('zoomed-in')
-    ylim([-50 50])
+    ylim([-5 5])
     cb = colorbar;
     cb.TickDirection = 'out';
     cb.Label.String = '10 log_{10}(spectral density) (dB)';
@@ -139,38 +133,42 @@ for ii = 1:fndex
 
     % make seismogram plot
     figure(2)
-    set(gcf, 'Unit', 'inches', 'Position', [0 1 11 5]);
+    set(gcf, 'Unit', 'inches', 'Position', [0 6 11 5]);
     clf
     ax21 = subplot('Position', [0.06 0.12 0.41 0.8]);
     hold on
-    scalingfactor = max(abs(xx), [], 'all') / dz * 2;
+    scalingfactor = indeks(maxk(max(abs(xx(:,9:end)), [], 1), 2), 2) / dz * 2;
     for kk = 1:size(xx, 2)
         plot(t, xx(:,kk) / scalingfactor + hmdep(kk), 'LineWidth', 1, 'Color', 'k')
     end
     xlabel('time (s)')
     ylabel('depth (m)')
-    title(sprintf('station: %s - %s', station, datestr(jdn)))
+    title(sprintf('station: %s - %s', station, dt_begin))
     grid on
     axis ij
     axis tight
+    ylim([hmdep(1)-dz hmdep(end)+dz])
     set(ax21, 'FontSize', 12, 'TickDir', 'out', 'Box', 'on')
 
     ax22 = subplot('Position', [0.56 0.12 0.41 0.8]);
     hold on
     xxf = xx;
     for kk = 1:size(xx, 2)
-        xxf(:,kk) = bandpass(xx(:,kk), 1/dt, 0.5, 2, 2, 1, 'butter', 'linear');
+        xxf(:,kk) = bandpass(detrend(xx(:,kk)) .* ...
+            shanning(size(xx,1), 0.01), 1/dt, 0.5, 5, 2, 1, 'butter', ...
+            'linear');
     end
-    scalingfactor = max(abs(xxf), [], 'all') / dz;
+    scalingfactor = indeks(maxk(max(abs(xxf(:,9:end)), [], 1), 2), 2) / dz * 1.5;
     for kk = 1:size(xx, 2)
         plot(t, xxf(:,kk) / scalingfactor + hmdep(kk), 'LineWidth', 1, 'Color', 'k')
     end
     xlabel('time (s)')
     ylabel('depth (m)')
-    title('0.5--2 Hz')
+    title('0.5--5 Hz')
     grid on
     axis ij
     axis tight
+    ylim([hmdep(1)-dz hmdep(end)+dz])
     set(ax22, 'FontSize', 12, 'TickDir', 'out', 'Box', 'on')
 
     set(gcf, 'Renderer', 'painters')
